13b4f1b0a11f13c1897290dd62aa5f4b78aa5020
İrem Akın Tunga <iremakin00@gmail.com>
2023-11-13 23:37:28 +0300

    adds handleComplete function

--- src/App.js
+++ src/App.js
@@ -6,21 +6,28 @@ import TaskHookForm from "./TaskHookForm";
 import PeopleForm from "./PeopleForm";
 import { initialTasks, initialTeam } from "./data";
 
-
 function App() {
   const [tasks, setTasks] = useState(initialTasks);
   const [team, setTeam] = useState(initialTeam);
 
   function handleTaskSubmit(yeniTask) {
-    setTasks([yeniTask, ...tasks])
+    setTasks([yeniTask, ...tasks]);
   }
 
   function handlePeopleSubmit(yeniKisi) {
-    setTeam([...team, yeniKisi])
+    setTeam([...team, yeniKisi]);
   }
 
   function handleComplete(id) {
-    console.log("tamamlama fonksiyonunu buraya yazın")
+    const completed = tasks.map((todo) => {
+      if (id === todo.id) {
+        return { ...todo, status: "yapıldı" };
+      } else {
+        return todo;
+      }
+    });
+    setTasks(completed);
+    console.log("tamamlama fonksiyonunu buraya yazın");
   }
 
   return (
@@ -59,7 +66,6 @@ function App() {
           </div>
         </div>
       </div>
-
     </div>
   );
 }
