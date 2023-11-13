2e6eafaf1202efa6ce66ead39da2517e9a532aa8
İrem Akın Tunga <iremakin00@gmail.com>
2023-11-13 23:49:39 +0300

    adds toastify to handleComplete function

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import TaskForm from "./TaskForm";
 import TaskHookForm from "./TaskHookForm";
 import PeopleForm from "./PeopleForm";
 import { initialTasks, initialTeam } from "./data";
+import { ToastContainer, toast, Flip } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   const [tasks, setTasks] = useState(initialTasks);
@@ -27,46 +29,62 @@ function App() {
       }
     });
     setTasks(completed);
+    toast.success("Liste tamamlandı!!");
     console.log("tamamlama fonksiyonunu buraya yazın");
   }
 
   return (
-    <div className="app">
-      <div className="formColumn">
-        <div className="form-container">
-          <h2>Yeni Task</h2>
-          {/* <TaskForm kisiler={team} submitFn={handleTaskSubmit} /> */}
-          <TaskHookForm kisiler={team} submitFn={handleTaskSubmit} />
-        </div>
+    <>
+      <div className="app">
+        <div className="formColumn">
+          <div className="form-container">
+            <h2>Yeni Task</h2>
+            {/* <TaskForm kisiler={team} submitFn={handleTaskSubmit} /> */}
+            <TaskHookForm kisiler={team} submitFn={handleTaskSubmit} />
+          </div>
 
-        <div className="form-container">
-          <h2>Yeni Kişi</h2>
-          <PeopleForm kisiler={team} submitFn={handlePeopleSubmit} />
-        </div>
-      </div>
-      <div className="columns">
-        <div className="column">
-          <h2 className="column-title">Yapılacaklar</h2>
-          <div className="column-list">
-            {tasks
-              .filter((t) => t.status === "yapılacak")
-              .map((t) => (
-                <Task key={t.id} taskObj={t} onComplete={handleComplete} />
-              ))}
+          <div className="form-container">
+            <h2>Yeni Kişi</h2>
+            <PeopleForm kisiler={team} submitFn={handlePeopleSubmit} />
           </div>
         </div>
-        <div className="column">
-          <h2 className="column-title">Tamamlananlar</h2>
-          <div className="column-list">
-            {tasks
-              .filter((t) => t.status === "yapıldı")
-              .map((t) => (
-                <Task key={t.id} taskObj={t} />
-              ))}
+        <div className="columns">
+          <div className="column">
+            <h2 className="column-title">Yapılacaklar</h2>
+            <div className="column-list">
+              {tasks
+                .filter((t) => t.status === "yapılacak")
+                .map((t) => (
+                  <Task key={t.id} taskObj={t} onComplete={handleComplete} />
+                ))}
+            </div>
+          </div>
+          <div className="column">
+            <h2 className="column-title">Tamamlananlar</h2>
+            <div className="column-list">
+              {tasks
+                .filter((t) => t.status === "yapıldı")
+                .map((t) => (
+                  <Task key={t.id} taskObj={t} />
+                ))}
+            </div>
           </div>
         </div>
       </div>
-    </div>
+      <ToastContainer
+        position="bottom-center"
+        autoClose={4000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+        transition={Flip}
+      />
+    </>
   );
 }
 
